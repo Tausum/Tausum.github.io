c3dbf5d6a692a3ebd27af5d0662d88cdf283dff3
tausum <tausum@qq.com>
2020-01-10 13:11:09 +0800

    2020年1月10日13:11:01  优化体验

--- js/convasworld.js
+++ js/convasworld.js
@@ -119,7 +119,7 @@ function loop() {
 
     RADIUS_SCALE = Math.min(RADIUS_SCALE, RADIUS_SCALE_MAX);
 
-    context.fillStyle = 'rgba(0,0,0,0.05)';
+    context.fillStyle = 'rgba(0,0,0,0.1)';
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
     for (i = 0, len = particles.length; i < len; i++) {
